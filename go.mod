304d02d64559daaba72a474b79b2088114767766
Andy D.K Lee <andy5090@naver.com>
2022-01-20 00:47:20 +0900

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/andy5090/nomadcoin
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/andy5090/nomadcoin/blockchain"
+)
+
+func main() {
+	chain := blockchain.GetBlockchain()
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Third Block")
+	chain.AddBlock("Fourth Block")
+
+	for _, block := range chain.AllBlocks() {
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %s\n", block.Hash)
+		fmt.Printf("Prev Hash: %s\n", block.PrevHash)
+	}
+}
