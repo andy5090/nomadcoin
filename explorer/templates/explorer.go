af917105209792cd71a87d5ca1266f89a0346650
Andy D.K Lee <andy5090@naver.com>
2022-01-28 14:37:02 +0900

    explorer done

---
+++ explorer/templates/explorer.go
@@ -0,0 +1,49 @@
+package explorer
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"text/template"
+
+	"github.com/andy5090/nomadcoin/blockchain"
+)
+
+const (
+	port        string = ":4000"
+	templateDir string = "explorer/templates/"
+)
+
+type homeData struct {
+	PageTitle string
+	Blocks    []*blockchain.Block
+}
+
+var templates *template.Template
+
+func home(rw http.ResponseWriter, r *http.Request) {
+	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
+	templates.ExecuteTemplate(rw, "home", data)
+}
+
+func add(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		templates.ExecuteTemplate(rw, "add", nil)
+	case "POST":
+		r.ParseForm()
+		data := r.Form.Get("blockData")
+		blockchain.GetBlockchain().AddBlock(data)
+		http.Redirect(rw, r, "/", http.StatusPermanentRedirect)
+	}
+
+}
+
+func Start() {
+	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
+	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
+	http.HandleFunc("/", home)
+	http.HandleFunc("/add", add)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
+}
--- main.go
+++ main.go
@@ -1,20 +1,7 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/andy5090/nomadcoin/blockchain"
-)
+import explorer "github.com/andy5090/nomadcoin/explorer/templates"
 
 func main() {
-	chain := blockchain.GetBlockchain()
-	chain.AddBlock("Second Block")
-	chain.AddBlock("Third Block")
-	chain.AddBlock("Fourth Block")
-
-	for _, block := range chain.AllBlocks() {
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %s\n", block.Hash)
-		fmt.Printf("Prev Hash: %s\n", block.PrevHash)
-	}
+	explorer.Start()
 }
