175c9239132cc35ae8b1bb0f61af9b5c8ba2d160
Andy D.K Lee <andy5090@naver.com>
2022-02-16 02:27:33 +0900

    persistence done

---
+++ blockchain/block.go
@@ -0,0 +1,50 @@
+package blockchain
+
+import (
+	"crypto/sha256"
+	"errors"
+	"fmt"
+
+	"github.com/andy5090/nomadcoin/db"
+	"github.com/andy5090/nomadcoin/utils"
+)
+
+type Block struct {
+	Data     string `json:"data"`
+	Hash     string `json:"hash"`
+	PrevHash string `json:"prevHash,omitempty"`
+	Height   int    `json:"height"`
+}
+
+func (b *Block) persist() {
+	db.SaveBlock(b.Hash, utils.ToBytes(b))
+}
+
+var ErrNotFound = errors.New("block not found")
+
+func (b *Block) restore(data []byte) {
+	utils.FromBytes(b, data)
+}
+
+func FindBlock(hash string) (*Block, error) {
+	blockBytes := db.Block(hash)
+	if blockBytes == nil {
+		return nil, ErrNotFound
+	}
+	block := &Block{}
+	block.restore(blockBytes)
+	return block, nil
+}
+
+func createBlock(data string, prevHash string, height int) *Block {
+	block := &Block{
+		Data:     data,
+		Hash:     "",
+		PrevHash: prevHash,
+		Height:   height,
+	}
+	payload := block.Data + block.PrevHash + fmt.Sprint(block.Height)
+	block.Hash = fmt.Sprintf("%x", sha256.Sum256([]byte(payload)))
+	block.persist()
+	return block
+}
---
+++ blockchain/chain.go
@@ -0,0 +1,61 @@
+package blockchain
+
+import (
+	"sync"
+
+	"github.com/andy5090/nomadcoin/db"
+	"github.com/andy5090/nomadcoin/utils"
+)
+
+type blockchain struct {
+	NewestHash string `json:"newestHash"`
+	Height     int    `json:"height"`
+}
+
+var b *blockchain
+var once sync.Once
+
+func (b *blockchain) restore(data []byte) {
+	utils.FromBytes(b, data)
+}
+
+func (b *blockchain) persist() {
+	db.SaveBlockchain(utils.ToBytes(b))
+}
+
+func (b *blockchain) AddBlock(data string) {
+	block := createBlock(data, b.NewestHash, b.Height+1)
+	b.NewestHash = block.Hash
+	b.Height = block.Height
+	b.persist()
+}
+
+func (b *blockchain) Blocks() []*Block {
+	var blocks []*Block
+	hashCursor := b.NewestHash
+	for {
+		block, _ := FindBlock(hashCursor)
+		blocks = append(blocks, block)
+		if block.PrevHash != "" {
+			hashCursor = block.PrevHash
+		} else {
+			break
+		}
+	}
+	return blocks
+}
+
+func Blockchain() *blockchain {
+	if b == nil {
+		once.Do(func() {
+			b = &blockchain{"", 0}
+			checkpoint := db.Checkpoint()
+			if checkpoint == nil {
+				b.AddBlock("Genesis")
+			} else {
+				b.restore(checkpoint)
+			}
+		})
+	}
+	return b
+}
